415acb24fd43784f14f1d9bd4a6d87e09cdecd8e
Sagar <sagartanksali3@gmail.com>
2024-04-07 22:59:02 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+
+
+        Runnable r = ()->{
+            System.out.println("Sample");
+            for(int i=1;i<5;i++){System.out.println(i);}
+        };
+        Thread t = new Thread(r);
+        t.start();
         System.out.println("Hello world!");
+        for(int i=1;i<5;i++){System.out.println(i);}
     }
 }
